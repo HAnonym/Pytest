cf1e138376965a8050c0312e5f6ff842d7032490
JAVAK <972223922@qq.com>
2021-09-07 15:47:03 +0800

    第一次学习

--- testcase/test_login.py
+++ testcase/test_login.py
@@ -4,26 +4,20 @@ import pytest
 
 
 class TestLogin:
-    def test_01_kong(self):
-        time.sleep(3)
+    def test_00_kong(self):
         print('哈哈哈哈')
 
     def test_01_kong(self):
-        time.sleep(3)
         print('哈哈哈哈')
 
     def test_02_kong(self):
-        time.sleep(3)
         print('CESHI 1')
 
     def test_03_kong(self):
-        time.sleep(3)
         print('还有谁')
 
     def test_04_kong(self):
-        time.sleep(3)
         print('56565')
 
     def test_05_kong(self):
-        time.sleep(3)
         print('哦156145')
